ef063fa92128a0a2d3b01c8164d25803596234f4
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-10-14 20:18:16 +0300

    Made the HomeView nicer added photos and better text

--- src/main/java/com/example/website/controller/HomeController.java
+++ src/main/java/com/example/website/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
 @RequestMapping("/")
-public class Test {
+public class HomeController {
 
     @GetMapping()
-    public String test() {
-        return "test";
+    public String getHomeView() {
+        return "Home";
     }
 }
