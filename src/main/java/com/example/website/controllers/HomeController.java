d284c45adfa38e0339c6c036a35d947765fe6224
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-10-15 13:55:48 +0300

    implemented email sending and formatting just need to fix up where it is sent

--- src/main/java/com/example/website/controllers/HomeController.java
+++ src/main/java/com/example/website/controllers/HomeController.java
@@ -1,15 +1,31 @@
 package com.example.website.controllers;
 
+import com.example.website.dtos.EmailDto;
+import com.example.website.services.EmailService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
 @RequestMapping("/")
 public class HomeController {
+    private final EmailService emailService;
+
+    public HomeController(EmailService emailService) {
+        this.emailService = emailService;
+    }
 
     @GetMapping()
-    public String getHomeView() {
+    public String getHomeView(Model model) {
+        model.addAttribute("emailDto", new EmailDto());
         return "Home";
+    }
+
+    @PostMapping()
+    public String handleEmailSending(EmailDto emailDto) {
+        emailService.sendEmail(emailDto.getName(), emailDto.getEmail(), emailDto.getSubject(), emailDto.getMessage());
+        return "redirect:/";
     }
 }
